f7af1df671d31273e7e93fc5d3a2d6866bacd252
SANJID ALAM <2403057@student.ruet.ac.bd>
2026-06-19 19:18:13 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ════════════════════════════════════════════════════════
 // CONFIG — your deployed Apps Script URL
 // ════════════════════════════════════════════════════════
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxLRmAKvW4U2fjoZ0Yr9v9k634jAqRXPqtBGasSV_GcevhaQOwPh6gqY6lSHvetbGIn/exec';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzXS3zocdTcLe_3rRv7lm3DdwOYEyWI3RhWBLEDQoMTtob8i0lTFLxo1g-fQi_oxmbb/exec';
 
 
 document.addEventListener('DOMContentLoaded', () => {
